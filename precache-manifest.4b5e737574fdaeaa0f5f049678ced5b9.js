00ec262842e065a1b63ba6a548345997d14ace59
dschoon <dschoon@gmail.com>
2019-11-10 21:49:24 -0600

    Updates

---
+++ precache-manifest.4b5e737574fdaeaa0f5f049678ced5b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8ba7a5becc2430437b07b764d46f16",
+    "url": "/react-waves/index.html"
+  },
+  {
+    "revision": "4582e77f1e9cb8541339",
+    "url": "/react-waves/static/css/main.05cfc9a3.chunk.css"
+  },
+  {
+    "revision": "cfa3dd9ab1bf1e93b4e1",
+    "url": "/react-waves/static/js/2.d19c650a.chunk.js"
+  },
+  {
+    "revision": "4582e77f1e9cb8541339",
+    "url": "/react-waves/static/js/main.3b7785da.chunk.js"
+  },
+  {
+    "revision": "10438e0551c1fe8273eb",
+    "url": "/react-waves/static/js/runtime-main.6f1bca1c.js"
+  },
+  {
+    "revision": "5aa39e7744227522390b8a59fb5250c3",
+    "url": "/react-waves/static/media/africa.5aa39e77.mp3"
+  },
+  {
+    "revision": "86e9eec564de69771b98afa8940760c1",
+    "url": "/react-waves/static/media/demo.86e9eec5.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-waves/precache-manifest.4f4da60d9e74811bddd54c687e2f322c.js"
+  "/react-waves/precache-manifest.4b5e737574fdaeaa0f5f049678ced5b9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-waves/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-waves/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
